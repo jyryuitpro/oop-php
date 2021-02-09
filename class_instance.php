a66bca2367dbfafa3c397fbe01bf0d5364b0fdae
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-02-09 16:36:27 +0900

    생성자(인스턴스 초기화)

--- class_instance.php
+++ class_instance.php
@@ -2,21 +2,21 @@
 
 class MyFileObject
 {
+    function __construct($filename)
+    {
+        $this->filename = $filename;
+    }
+
     function isFile()
     {
         return is_file($this->filename);
     }
 }
 
-$file = new MyFileObject();
-$file->filename = 'data.txt';
+$file = new MyFileObject('data.txt');
+//$file->filename = 'data.txt';
 var_dump($file->isFile());
 var_dump($file->filename);
-
-$file2 = new MyFileObject();
-$file2->filename = 'data2.txt';
-var_dump($file2->isFile());
-var_dump($file2->filename);
 
 /**
  * MyFileObject : Class
