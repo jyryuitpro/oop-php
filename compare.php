a60bc9624323d6b322f3a6bca64a01eefa224b5a
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-02-09 13:39:36 +0900

    함수와 객체 3

--- compare.php
+++ compare.php
@@ -12,4 +12,15 @@ var_dump($file->isFile());
 var_dump($file->isDir());
 var_dump($file->fread($file->getSize()));
 $file->fwrite(rand(1, 100));
+
+$file2 = new SplFileObject('data2.txt');
+var_dump($file2->isFile());
+var_dump($file2->isDir());
+var_dump($file2->fread($file2->getSize()));
+$file2->fwrite(rand(1, 100));
+
+// SplFileObject : Class
+// $file, $file2 : Instance
+// isFile, isDir, fread, getSize, fwrite : Method(function)
+// data.txt, data2.txt : state
 ?>
