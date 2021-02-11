4abf99c605480a7723beaf23d9354c8be3913e00
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-02-11 14:16:25 +0900

    클래스로딩과 네임스페이스 1

---
+++ autoload/greeting.php
@@ -0,0 +1,13 @@
+<?php
+
+class Hi
+{
+
+    /**
+     * Hi constructor.
+     */
+    public function __construct()
+    {
+        echo 'hi';
+    }
+}
---
+++ autoload/main.php
@@ -0,0 +1,4 @@
+<?php
+require_once 'greeting.php';
+
+new Hi();
