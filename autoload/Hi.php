f785ed2a68ca830e2f15252840b4aa2cb082c223
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-02-13 09:02:45 +0900

    클래스로딩과 네임스페이스 3 (namespace)

--- autoload/Hi.php
+++ autoload/Hi.php
@@ -1,8 +1,7 @@
 <?php
-
+namespace greeting\en;
 class Hi
 {
-
     /**
      * Hi constructor.
      */
@@ -11,3 +10,25 @@ class Hi
         echo '<h1>hi</h1>';
     }
 }
+
+namespace greeting\ko;
+class Hi
+{
+    /**
+     * Hi constructor.
+     */
+    public function __construct()
+    {
+        echo '<h1>안녕</h1>';
+    }
+}
+//use \greeting\en, \greeting\ko;
+//new en\Hi();
+//new ko\Hi();
+
+use \greeting\en\Hi as HiEn;
+use \greeting\ko\Hi as HiKo;
+new HiEn();
+new HiKo();
+
+
