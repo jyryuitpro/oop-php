ae56ae8e4f8d7d4a55a75506e8d73cd46f749e82
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-02-14 18:32:39 +0900

    Interface 3 (monolog의 사례)

--- composerApp/index.php
+++ composerApp/index.php
@@ -5,11 +5,20 @@ require_once __DIR__.'/vendor/autoload.php';
 
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
+use Monolog\Handler\NativeMailerHandler;
 
 // create a log channel
 $log = new Logger('name');
-$log->pushHandler(new StreamHandler(__DIR__.'/app.log', Logger::WARNING));
+//$log->pushHandler(new StreamHandler(__DIR__.'/app.log', Logger::WARNING));
+//$log->pushHandler(new StreamHandler(__DIR__.'/app.log', Logger::ERROR));
+$log->pushHandler(new StreamHandler(__DIR__.'/app.log', Logger::ERROR));
+$log->pushHandler(new NativeMailerHandler(
+    'jy.ryu.itpro@gmail.com',
+    'Emergency!!!',
+    'out@system.com',
+    Logger::EMERGENCY));
 
 // add records to the log
 $log->warning('EGO');
 $log->error('ING');
+$log->emergency('Emergency');
